463a32c8bde10090782460d05c963973419e2947
gmchen188 <gmchen188@outlook.com>
2023-05-24 23:12:58 +0800

    Add files via upload

--- main.tf
+++ main.tf
@@ -7,12 +7,15 @@ terraform {
       version = "~> 3.0.2"
     }
   }
+  
  cloud {
     organization = "em-terraform-cloud"
     workspaces {
       name = "learn-terraform-azure"
     }
   }
+
+}
 
 provider "azurerm" {
   features {}
@@ -35,5 +38,5 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-}
+
 
