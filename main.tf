891e3098055d821555d504de7d2427a74580c6ef
gmchen188 <gmchen188@outlook.com>
2023-05-24 23:43:17 +0800

    Add files via upload

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "~> 3.57.0"
     }
   }
-  
+
  cloud {
     organization = "em-terraform-cloud"
     workspaces {
