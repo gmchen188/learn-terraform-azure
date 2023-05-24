2c4c3f58692a6ef282b3b64df43094bbc816a740
gmchen188 <gmchen188@outlook.com>
2023-05-24 16:46:41 +0800

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,21 +1,19 @@
 # Configure the Azure provider
 terraform {
+  required_version = ">= 1.1.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
   }
-
-  required_version = ">= 1.1.0"
-}
-
  cloud {
     organization = "em-terraform-cloud"
     workspaces {
       name = "learn-terraform-azure"
     }
   }
+}
 
 provider "azurerm" {
   features {}
@@ -37,3 +35,4 @@ resource "azurerm_virtual_network" "vnet" {
   location            = "eastasia"
   resource_group_name = azurerm_resource_group.rg.name
 }
+
