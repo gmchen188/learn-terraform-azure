84cf0e0075e1c03bb3a1e4bb90cab54216adede8
gmchen188 <gmchen188@outlook.com>
2023-05-24 16:10:58 +0800

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,39 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+ cloud {
+    organization = "em-terraform-cloud"
+    workspaces {
+      name = "learn-terraform-azure"
+    }
+  }
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "eastasia"
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team = "CloudOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastasia"
+  resource_group_name = azurerm_resource_group.rg.name
+}
